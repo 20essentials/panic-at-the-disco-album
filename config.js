9157c4d8dc02cb9c1b1d4f5a4a249c52fd5c3362
DominicDev <s0l0estudi4r@gmail.com>
2026-02-26 13:17:42 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PANIC AT THE DISCO';
+export let nameSongs = [
+	"HOUSE OF MEMORIES", /*1*/
+	"HIGH HOPES", /*2*/
+	"THIS IS GOSPEL", /*3*/
+	"BALLAD OF MONA LISA", /*4*/
+	"LA DEVOTEE", /*5*/
+	"NEARLY WITCHES", /*6*/
+	"NEW PERSPECTIVE", /*7*/
+	"GOLDEN DAYS", /*8*/
+	"TURN OFF THE LIGHTS", /*9*/
+	"TIME TO DANCE", /*10*/
+	"FAR TOO YOUNG TO DIE", /*11*/
+	"C'MON", /*12*/
+	"TRADE MISTAKES", /*13*/
+	"NICOTINE", /*14*/
+	"CASUAL AFFAIR", /*15*/
+	"DON'T THREATEN ME ", /*16*/
+	"CRAZY = GENIUS", /*17*/
+	"COLLAR FULL", /*18*/
+	"READY TO GO", /*19*/
+	"LET'S KILL TONIGHT", /*20*/
+]
